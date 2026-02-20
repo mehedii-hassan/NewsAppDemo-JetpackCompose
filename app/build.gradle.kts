8db1bc32c46799b935d5b65742fb67f137239b7a
Mehedi_Hassan <mehedii.swe@gmail.com>
2026-02-20 16:22:16 +0600

    ✨ feat: add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //Splash Api
+    implementation("androidx.core:core-splashscreen:1.2.0")
 }
